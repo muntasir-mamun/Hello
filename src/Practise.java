95ac1ee125a1139996717e15ae3c10e429eeba0f
muntasir-mamun <mdmunnakhn6195@gmail.com>
2018-02-26 02:13:00 +0600

    Changes

--- src/Practise.java
+++ src/Practise.java
@@ -3,6 +3,14 @@ public class Practise {
 
 	public static void main(String[] args) {
 		
+		int a =2453324;
+		int b = 123156;
+		
+		int sum = a+b;
+		
+		
+		System.out.println("sum="+sum);
+		
 
 	}
 
